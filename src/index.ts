d0db8c32a14b4be9d6380fd67de6de4ea59151ad
mikeangeloo <mikeangeloo87@outlook.com>
2023-08-07 18:36:17 -0500

    primer comentario

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import * as admin from 'firebase-admin'
+import {ApolloError, ApolloServer, ValidationError, gql} from 'apollo-server'
+
+const serviceAccount = require('../service-account.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+interface User {
+    id: string;
+    name: string;
+    screenName: string;
+    statusesCount: number;
+  }
+  
+  interface Tweet {
+    id: string;
+    likes: number;
+    text: string;
+    userId: string;
+  }
+
+const typeDefs = gql`
+    # Twitter User
+    type User {
+        id: ID!
+        screenName: String!
+        statusesCount: Int!
+        tweets: [Tweets]!
+    }
+
+    #A Tweet Oject
+    type Tweets {
+        id: ID!
+        text: String!
+        userId: String!
+        user: User!
+        likes: Int!
+    }
+
+    type Query {
+        tweets: [Tweets]
+        user(id: String!): User
+    }
+`
+
+const resolvers = {
+    User: {
+        async tweets(user) {
+            try {
+                const userTweets = await admin.firestore().collection('tweets').where('userId', '==', user.id).get()
+                return userTweets.docs.map(tweet => tweet.data()) as Tweet[]
+            } catch (error) {
+                throw new ApolloError(error)
+            }
+        }
+    },
+    Tweets: {
+        async user(tweet) {
+            try {
+                const tweetAuthor = await admin.firestore().doc(`users/${tweet.userId}`).get()
+                return tweetAuthor.data() as User
+            } catch (error) {
+                throw new ApolloError(error)
+            }
+        }
+    },
+    Query: {
+        async tweets () {
+            const tweets = await admin.firestore().collection('tweets').get()
+            return tweets.docs.map(tweet => tweet.data() as Tweet[])
+        },
+        async user(_: null, args: {id: string}) {
+            try {
+                const userDoc = await admin.firestore().doc(`users/${args.id}`).get()
+                const user = userDoc.data() as User | undefined
+                return user || new ValidationError('User ID not found')
+            } catch (error) {
+                throw new ApolloError(error)
+            }
+        }
+    }   
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({port: 3000}).then(({url}) => {
+    console.log(`Server ready at ${url}`)
+})
